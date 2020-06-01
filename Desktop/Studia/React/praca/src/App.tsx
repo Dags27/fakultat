66a1152f09d88f6db0c44a4c4e6c17aab6dcc12f
Dags27 <dags.slizewski@gmail.com>
2020-06-01 18:07:17 +0200

    v1

---
+++ Desktop/Studia/React/praca/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Switch, Route, BrowserRouter, Link } from 'react-router-dom';
+import PostList from './posts/posts';
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/post">About</Link></li>
+        </ul>
+      </nav>
+          <Switch>
+
+            <Route path="/post" component={PostList}/>
+          </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ Desktop/Studia/React/praca/src/posts/posts.tsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import PostData from '../json/posts.json';
+import '../style/style.css';
+
+class PostList extends Component {
+    render(){
+    return (
+    <div>
+        <h1>Hello There</h1>
+        {PostData.map((e)=>{
+            return( 
+                <div>
+                    <h1 onClick>{e.title}</h1>
+                    {e.body}
+                </div>
+            )
+        })}
+    </div>
+  );
+}
+}
+
+export default PostList;
